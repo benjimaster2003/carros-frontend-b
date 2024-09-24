4c63a73e1a6325a78ed472280617e4fa48b9a87c
Jerson <benjimaster2003@gmail.com>
2024-09-24 14:05:53 -0500

    carga inicial del examen

---
+++ src/main/java/pe/edu/cibertec/carros_frontend_b/CarrosFrontendBApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.carros_frontend_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrosFrontendBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosFrontendBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/carros_frontend_b/CarrosFrontendBApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.carros_frontend_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosFrontendBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
